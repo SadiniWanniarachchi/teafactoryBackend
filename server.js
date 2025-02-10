0476deee786ca97c51f603f46a07b80ced38de91
SadiniWanniarachchi <sadiiw17@gmail.com>
2025-02-10 08:56:27 +0530

    feat-done

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cloudinary = require('cloudinary').v2;
 
 // Middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],  // Allow both origins
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],  // Allow both origins
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
 };
