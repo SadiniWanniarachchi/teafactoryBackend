3985d210da33f07488dcd38578092beaebc9f489
SadiniWanniarachchi <sadiiw17@gmail.com>
2025-02-17 09:15:07 +0530

    feat-make validations in batch number

--- models/QualityCheck.js
+++ models/QualityCheck.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const QualityCheckSchema = new mongoose.Schema({
-  batchNumber: { type: String, required: true },
+  batchNumber: {
+    type: String,
+    required: true,
+    match: [/^(?=.*[A-Z])(?=.*\d)[A-Z0-9]+$/, 'Batch number must contain at least one uppercase letter and one number'],
+  },
   inspectionDate: { type: Date, required: true },
   status: { type: String, enum: ['Passed', 'Failed'], required: true },
 });
